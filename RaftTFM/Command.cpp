0db5b3dfc53227b7d347a4c39a7ee9b0e8204676
sanescamo <sanescamo@hotmail.com>
2021-04-02 13:45:38 +0200

    Added command

--- RaftTFM/Command.cpp
+++ RaftTFM/Command.cpp
@@ -1,1 +1,19 @@
 #include "Command.h"
+
+void Command::set_state_machime_command(uint32_t state_machine_command_)
+{
+}
+
+uint32_t Command::get_state_machime_command()
+{
+	return uint32_t();
+}
+
+void Command::set_term_when_entry_was_received_by_leader(uint32_t state_machine_command_)
+{
+}
+
+uint32_t Command::get_term_when_entry_was_received_by_leader()
+{
+	return uint32_t();
+}
--- RaftTFM/Command.h
+++ RaftTFM/Command.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+
 class Command
 {
+private:
+	uint32_t state_machine_command_;
+	uint32_t term_when_entry_was_received_by_leader_;
+public:
+	void set_state_machime_command(uint32_t state_machine_command_);
+	uint32_t get_state_machime_command();
+
+	void set_term_when_entry_was_received_by_leader(uint32_t state_machine_command_);
+	uint32_t get_term_when_entry_was_received_by_leader();
 };
 
