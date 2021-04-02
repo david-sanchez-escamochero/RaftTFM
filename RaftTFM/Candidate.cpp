2013333cee8192685ae9d172bcda9570fca18c9d
sanescamo <sanescamo@hotmail.com>
2021-04-02 19:04:17 +0200

    Candidate RPC RV

--- RaftTFM/Candidate.cpp
+++ RaftTFM/Candidate.cpp
@@ -38,7 +38,8 @@ void Candidate::send_request_vote_to_all_servers()
 		for (uint32_t count = 0; count < NUM_SERVERS; count++) 
 		{
 			{
-				//std::lock_guard<std::mutex> locker_new_state(mu_candidate_);
+				std::lock_guard<std::mutex> locker_new_state(mu_candidate_);
+				
 
 				if ((!there_is_leader_) && (!have_to_die_)) {
 
@@ -87,57 +88,49 @@ void Candidate::send(RPC* rpc, unsigned short port, std::string sender, std::str
 	((Server*)server_)->send(rpc, port, sender, action, receiver);
 }
 
-void Candidate::receive(RPC* rpc)
+void Candidate::dispatch_append_entry(RPC* rpc)
 {
-	//std::lock_guard<std::mutex> locker_new_state(mu_candidate_);
+	// And its terms is equal or highest than mine... 
+	if (rpc->append_entry.argument_term_ >= ((Server*)server_)->get_current_term()) {
+		Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") [Accepted]received an append_entry[term:" + std::to_string(rpc->append_entry.argument_term_) + " >= current_term:" + std::to_string(((Server*)server_)->get_current_term()) + "]\r\n");
+		// Inform server that state has changed to follower.  
+		((Server*)server_)->set_new_state(StateEnum::follower_state);
 
-	// If I receive an append_entry(// Another server establishes itself as a leader. )
-	if (rpc->rpc_type == RPCTypeEnum::rpc_append_entry)
-	{
-		
-		// And its terms is equal or highest than mine... 
-		if (rpc->append_entry.argument_term_ >= ((Server*)server_)->get_current_term()) {
-			Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") [Accepted]received an append_entry[term:" +std::to_string(rpc->append_entry.argument_term_)+ " >= current_term:"+std::to_string(((Server*)server_)->get_current_term())+"]\r\n");
-			// Inform server that state has changed to follower.  
-			((Server*)server_)->set_new_state(StateEnum::follower_state);
+		rpc->rpc_direction = RPCDirection::rpc_out_result;
+		rpc->append_entry.result_success_ = true;
+		rpc->server_id_origin = ((Server*)server_)->get_server_id();
+		rpc->server_id_target = rpc->append_entry.argument_leader_id_;
 
-			rpc->rpc_direction = RPCDirection::rpc_out_result;
-			rpc->append_entry.result_success_ = true;
-			rpc->server_id_origin = ((Server*)server_)->get_server_id();
-			rpc->server_id_target = rpc->append_entry.argument_leader_id_;
-
-			send(rpc,
-				PORT_BASE + RECEIVER_PORT + rpc->append_entry.argument_leader_id_,
-				std::string(SERVER) + "(C)." + std::to_string(((Server*)server_)->get_server_id()),
-				std::string(APPEND_ENTRY) + std::string("(") + std::string(RESULT) + std::string(")"),
-				std::string(SERVER) + "(C)." + std::to_string(rpc->append_entry.argument_leader_id_)
-			);
-			have_to_die_ = true;
-			there_is_leader_ = true;
-		}
-		// Reject...
-		else {
-			Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") [Rejected] received an append_entry[term:" + std::to_string(rpc->append_entry.argument_term_) + " < current_term:" + std::to_string(((Server*)server_)->get_current_term()) + "]\r\n");
-			rpc->rpc_direction = RPCDirection::rpc_out_result;
-			rpc->append_entry.result_success_ = false;
-
-			send(rpc,
-				PORT_BASE + RECEIVER_PORT + rpc->append_entry.argument_leader_id_,
-				std::string(SERVER) + "(C)." + std::to_string(((Server*)server_)->get_server_id()),
-				std::string(APPEND_ENTRY) + std::string("(") + std::string(RESULT) + std::string(")"),
-				std::string(SERVER) + "(C)." + std::to_string(rpc->append_entry.argument_leader_id_)
-			);
-		}
+		send(rpc,
+			PORT_BASE + RECEIVER_PORT + rpc->append_entry.argument_leader_id_,
+			std::string(SERVER) + "(C)." + std::to_string(((Server*)server_)->get_server_id()),
+			std::string(APPEND_ENTRY) + std::string("(") + std::string(RESULT) + std::string(")"),
+			std::string(SERVER) + "(C)." + std::to_string(rpc->append_entry.argument_leader_id_)
+		);
+		have_to_die_ = true;
+		there_is_leader_ = true;
 	}
+	// Reject...
+	else {
+		Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") [Rejected] received an append_entry[term:" + std::to_string(rpc->append_entry.argument_term_) + " < current_term:" + std::to_string(((Server*)server_)->get_current_term()) + "]\r\n");
+		rpc->rpc_direction = RPCDirection::rpc_out_result;
+		rpc->append_entry.result_success_ = false;
 
+		send(rpc,
+			PORT_BASE + RECEIVER_PORT + rpc->append_entry.argument_leader_id_,
+			std::string(SERVER) + "(C)." + std::to_string(((Server*)server_)->get_server_id()),
+			std::string(APPEND_ENTRY) + std::string("(") + std::string(RESULT) + std::string(")"),
+			std::string(SERVER) + "(C)." + std::to_string(rpc->append_entry.argument_leader_id_)
+		);
+	}
+}
 
-	// If I receive a request vote(// Another server is faster than I am. )
-	if (rpc->rpc_type == RPCTypeEnum::rpc_append_request_vote)
-	{
-		
+void Candidate::dispatch_request_vote(RPC* rpc) 
+{
+	if (rpc->rpc_direction == RPCDirection::rpc_in_invoke) {
 		// And its terms is equal or highest than mine... 
 		if (rpc->request_vote.argument_term_ >= ((Server*)server_)->get_current_term()) {
-			Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") [Accepted]received an request_vote[term:"+std::to_string(rpc->request_vote.argument_term_)+" >= current_term:" + std::to_string(((Server*)server_)->get_current_term()) + "]\r\n");
+			Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") [Accepted]received an request_vote[term:" + std::to_string(rpc->request_vote.argument_term_) + " >= current_term:" + std::to_string(((Server*)server_)->get_current_term()) + "]\r\n");
 			// Inform server that state has changed to follower.  
 			((Server*)server_)->set_new_state(StateEnum::follower_state);
 
@@ -169,23 +162,53 @@ void Candidate::receive(RPC* rpc)
 				std::string(SERVER) + "(C)." + std::to_string(rpc->append_entry.argument_leader_id_)
 			);
 		}
+
 	}
-
-
-	// If I wins election. 	
-	else if ((rpc->rpc_type == RPCTypeEnum::rpc_append_request_vote) && ((bool)rpc->request_vote.result_term_ == true)) {
-		receive_votes_++;
-		if (receive_votes_ >= MAJORITY) {
-			Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") I have received just mayority of request vote: " + std::to_string(receive_votes_) + "\r\n");
-			((Server*)server_)->set_new_state(StateEnum::leader_state);
-			there_is_leader_ = true;
+	// rpc_out_result
+	else if ((rpc->rpc_direction == RPCDirection::rpc_out_result)) {
+		// If someone voted for me. 
+		if ((bool)rpc->request_vote.result_term_ == true) {
+			receive_votes_++;
+			// If I wins election. 	
+			if (receive_votes_ >= MAJORITY) {
+				Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") I have received just mayority of request vote: " + std::to_string(receive_votes_) + "\r\n");
+				((Server*)server_)->set_new_state(StateEnum::leader_state);
+				there_is_leader_ = true;
+			}
+		}
+		// If I was rejected. 
+		else {
+			Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") Rejected request voted\r\n");
 		}
 	}
-	// Another server establishes itself as a leader. 
-	else if (rpc->rpc_type == RPCTypeEnum::rpc_append_heart_beat) {
-		Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") received heart_beat\r\n");
-		((Server*)server_)->set_new_state(StateEnum::follower_state);
-		there_is_leader_ = true;
+}
+
+void Candidate::dispatch_append_heart_beat(RPC* rpc) 
+{
+	Log::trace("(Candidate." + std::to_string(((Server*)server_)->get_server_id()) + ") received heart_beat\r\n");
+	((Server*)server_)->set_new_state(StateEnum::follower_state);
+	there_is_leader_ = true;
+}
+
+void Candidate::receive(RPC* rpc)
+{
+	std::lock_guard<std::mutex> locker_new_state(mu_candidate_);
+
+	if ((!there_is_leader_) && (!have_to_die_)) {
+		// If I receive an append_entry(// Another server establishes itself as a leader. )
+		if (rpc->rpc_type == RPCTypeEnum::rpc_append_entry)
+		{
+			dispatch_append_entry(rpc);
+		}
+		// If I receive a request vote(// Another server is faster than I am. )
+		else if (rpc->rpc_type == RPCTypeEnum::rpc_append_request_vote)
+		{
+			dispatch_request_vote(rpc);
+		}
+		// Another server establishes itself as a leader. 
+		else if (rpc->rpc_type == RPCTypeEnum::rpc_append_heart_beat) {
+			dispatch_append_heart_beat(rpc);
+		}
 	}
 }
 
